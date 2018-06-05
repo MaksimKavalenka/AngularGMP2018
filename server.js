29b56b30659b9083ea5e66cac3e605b9f94e996a
Maksim Kavalenka <maksim.kavalenka@gmail.com>
2018-06-06 01:25:28 +0300

    Server update

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import * as express from 'express';
-import * as path from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/AngularGMP2018'));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/AngularGMP2018/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
