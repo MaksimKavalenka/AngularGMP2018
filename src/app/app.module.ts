fcc9b175e4423be447616ae23a545afa686fad7e
Maksim Kavalenka <maksim.kavalenka@gmail.com>
2018-06-09 00:43:20 +0300

    Task1: Create 'breadcrumbs' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BreadcrumbsComponent,
     HeaderComponent
   ],
   imports: [
