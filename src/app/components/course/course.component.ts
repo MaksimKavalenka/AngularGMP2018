1a058d6feb01bce75b96736aea1c4caf245b66b9
Maksim Kavalenka <maksim.kavalenka@gmail.com>
2018-06-12 23:19:13 +0300

    Task1: Classes implementation

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -8,69 +8,46 @@ import { Course } from '../../entities/course';
 })
 export class CourseComponent implements OnInit {
 
-  private courses: Course[] = [{
-    id: '1',
-    title: 'Video Course 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
+  private courses: Course[] = [
+    new Course(
+      '1', 'Video Course 1', 88, new Date('05.23.2018'),
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
       'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute ' +
       'irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat ' +
       'non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    duration: 88,
-    creationDate: new Date('05.23.2018'),
-  }, {
-    id: '2',
-    title: 'Video Course 2',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
+    ),
+    new Course(
+      '2', 'Video Course 2', 27, new Date('06.10.2018'),
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
       'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute ' +
       'irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat ' +
       'non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    duration: 27,
-    creationDate: new Date('06.10.2018'),
-  }, {
-    id: '3',
-    title: 'Video Course 3',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
+    ),
+    new Course(
+      '3', 'Video Course 3', 70, new Date('07.14.2018'),
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
       'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute ' +
       'irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat ' +
       'non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    duration: 70,
-    creationDate: new Date('07.14.2018'),
-  }, {
-    id: '4',
-    title: 'Video Course 4',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
+    ),
+    new Course(
+      '4', 'Video Course 4', 46, new Date('07.16.2018'),
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
       'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute ' +
       'irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat ' +
       'non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    duration: 46,
-    creationDate: new Date('07.16.2018'),
-  }, {
-    id: '5',
-    title: 'Video Course 5',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
+    ),
+    new Course(
+      '5', 'Video Course 5', 30, new Date('08.21.2018'),
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
       'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute ' +
       'irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat ' +
       'non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    duration: 30,
-    creationDate: new Date('08.21.2018'),
-  }];
+    ),
+  ];
 
   public constructor() { }
 
   public ngOnInit() { }
-
-  public getDuration(course: Course): string {
-    const duration: number[] = [];
-    duration[0] = Math.floor(course.duration / 60);
-    duration[1] = course.duration % 60;
-
-    if (!duration[0]) {
-      return `${duration[1]}min`;
-    }
-    if (!duration[1]) {
-      return `${duration[0]}h`;
-    }
-    return `${duration[0]}h ${duration[1]}min`;
-  }
 
 }
--- src/app/entities/course.ts
+++ src/app/entities/course.ts
@@ -1,7 +1,37 @@
-export interface Course {
+export interface ICourse {
   id: string;
   title: string;
   duration: number;
   creationDate: Date;
   description: string;
+}
+
+export class Course implements ICourse {
+  public id: string;
+  public title: string;
+  public duration: number;
+  public creationDate: Date;
+  public description: string;
+
+  public constructor(id: string, title: string, duration: number, creationDate: Date, description: string) {
+    this.id = id;
+    this.title = title;
+    this.duration = duration;
+    this.creationDate = creationDate;
+    this.description = description;
+  }
+
+  public getDuration(): string {
+    const duration: number[] = [];
+    duration[0] = Math.floor(this.duration / 60);
+    duration[1] = this.duration % 60;
+
+    if (!duration[0]) {
+      return `${duration[1]}min`;
+    }
+    if (!duration[1]) {
+      return `${duration[0]}h`;
+    }
+    return `${duration[0]}h ${duration[1]}min`;
+  }
 }
